8993c5ea0df74df7c2336e0de14a10bcd37934b6
Dalvin Matthews <dalvinjmatthews@gmail.com>
2022-10-17 20:54:49 -0400

    updated package

--- rest/response.go
+++ rest/response.go
@@ -4,5 +4,6 @@ type Response struct {
 	Status   int               `json:"status"`
 	Headers  map[string]string `json:"headers,omitempty"`
 	Data     any               `json:"data,omitempty"`
+	Error    error             `json:"error,omitempty"`
 	MetaData any               `json:"metaData,omitempty"`
 }
