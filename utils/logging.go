4e45f5a9770f5cd07242c822030e6a91e4804e5d
HeBeCoding <dalvinjmatthews@gmail.com>
2023-06-08 23:12:00 -0400

    added interface to logger

--- utils/logging.go
+++ utils/logging.go
@@ -4,6 +4,23 @@ import (
 	"go.uber.org/zap"
 )
 
+type LoggerInterface interface {
+	Debug(args ...interface{})
+	Info(args ...interface{})
+	Warn(args ...interface{})
+	Error(args ...interface{})
+	Panic(args ...interface{})
+	Fatal(args ...interface{})
+	Debugf(template string, args ...interface{})
+	Infof(template string, args ...interface{})
+	Warnf(template string, args ...interface{})
+	Errorf(template string, args ...interface{})
+	Panicf(template string, args ...interface{})
+	Fatalf(template string, args ...interface{})
+	With(args ...interface{}) *Logger
+	Sync() error
+}
+
 type Logger struct {
 	*zap.SugaredLogger
 }
